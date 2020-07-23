1f314aa2c32f9e2df8b509bc27de9546e75ff02c
iwasnevergivenaname <hannah.mace93@gmail.com>
2020-07-23 12:30:51 -0700

    connect js files to front end. can submit form data

--- results.js
+++ results.js
@@ -3,4 +3,11 @@ console.log(resultDiv);
 
 new URLSearchParams(window.location.search).forEach((value, name) => {
     console.log(name, value);
+
+    let resultElement = document.createElement("p");
+    resultElement.textContent = name + ": " + value;
+
+
+    resultDiv.appendChild(resultElement);
+
 })
